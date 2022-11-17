79a18baa2eada3a584b0a097b3685052cdc3e5ce
Luis Jimenez <luisjimenez@Luiss-Air.attlocal.net>
2022-11-16 20:53:09 -0600

    Working on the models

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('moongose');
+
+const userSchema = new Schema(
+  {
+    id: {
+      type: Number
+    },
+    firstName: {
+      type: String,
+      require: true,
+    },
+    lastName: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      required: 'Email address is required',
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    }
+  }
+)
