8400f7d33d2227efeb5400ef0ce687b933eea1b8
Luis Jimenez <luisjimenez@Luiss-MacBook-Air.local>
2022-11-20 16:27:53 -0600

    Login and sign up

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -1,4 +1,6 @@
 const { User, Restaurant } = require('../models');
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require('../utils/auth')
 
 const resolvers = {
   Query: {
--- utils/auth.js
+++ utils/auth.js
@@ -27,8 +27,8 @@ module.exports = {
 
     return req;
   },
-  signToken: function ({ email, username, _id }) {
-    const payload = { email, username, _id };
+  signToken: function ({ email, username }) {
+    const payload = { email, username };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 
