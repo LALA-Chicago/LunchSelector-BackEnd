b2e1f42207d60d04908cac76c2fc6c1ca514a396
Angel Escobar <escobar@Angels-MacBook-Pro-162.local>
2022-11-21 20:40:09 -0600

    typo fix

--- utils/auth.js
+++ utils/auth.js
@@ -27,8 +27,8 @@ module.exports = {
 
     return req;
   },
-  signToken: function ({ email, username }) {
-    const payload = { email, username };
+  signToken: function ({ email, username, _id }) {
+    const payload = { email, username, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 
